dc746feead0a2e0478582eb5ca8f9bf38b4a16e1
bldrchd <asya.chakarova@sap.com>
2019-02-25 11:49:28 +0200

    first commit

---
+++ src/main/java/com/sap/calcacademy/calculatordb/CalculationOps.java
@@ -0,0 +1,20 @@
+package com.sap.calcacademy.calculatordb;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import com.sap.calcacademy.calculatordb.model.Calculation;
+
+public class CalculationOps {
+    private final EntityManager em;
+
+    public CalculationOps(EntityManager em) {
+        this.em = em;
+    }
+    public List<Calculation> getAll() {
+        Query q = em.createNamedQuery("Calculation.findAll");
+        List<Calculation> all = q.getResultList();
+        return all;
+    }
+}
---
+++ src/main/java/com/sap/calcacademy/calculatordb/MyQuery.java
@@ -0,0 +1,5 @@
+package com.sap.calcacademy.calculatordb;
+
+public class MyQuery {
+
+}
